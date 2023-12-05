151209bf6968233e7ccff7b6d506c8f3e2f25489
ahmadjalal1122 <ahmadjalal110101@gmail.com>
2023-12-06 01:34:17 +0500

    folder changed

--- backend/Encryption/DES.py
+++ backend/Encryption/DES.py
@@ -1,217 +1,8 @@
-# from itertools import product
+from flask import Flask, request
+from flask_cors import CORS  # Import the CORS extension
 
-# # Initial Permutation Table
-# initial_permutation = [
-#     58, 50, 42, 34, 26, 18, 10, 2,
-#     60, 52, 44, 36, 28, 20, 12, 4,
-#     62, 54, 46, 38, 30, 22, 14, 6,
-#     64, 56, 48, 40, 32, 24, 16, 8,
-#     57, 49, 41, 33, 25, 17, 9, 1,
-#     59, 51, 43, 35, 27, 19, 11, 3,
-#     61, 53, 45, 37, 29, 21, 13, 5,
-#     63, 55, 47, 39, 31, 23, 15, 7
-# ]
-
-# # Final Permutation Table
-# final_permutation = [
-#     40, 8, 48, 16, 56, 24, 64, 32,
-#     39, 7, 47, 15, 55, 23, 63, 31,
-#     38, 6, 46, 14, 54, 22, 62, 30,
-#     37, 5, 45, 13, 53, 21, 61, 29,
-#     36, 4, 44, 12, 52, 20, 60, 28,
-#     35, 3, 43, 11, 51, 19, 59, 27,
-#     34, 2, 42, 10, 50, 18, 58, 26,
-#     33, 1, 41, 9, 49, 17, 57, 25
-# ]
-
-# # Expansion D-box Table
-# expansion_d_box = [
-#     32, 1, 2, 3, 4, 5, 4, 5,
-#     6, 7, 8, 9, 8, 9, 10, 11,
-#     12, 13, 12, 13, 14, 15, 16, 17,
-#     16, 17, 18, 19, 20, 21, 20, 21,
-#     22, 23, 24, 25, 24, 25, 26, 27,
-#     28, 29, 28, 29, 30, 31, 32, 1
-# ]
-
-# # P-box permutation
-# p_box_permutation = [
-#     16, 7, 20, 21,
-#     29, 12, 28, 17,
-#     1, 15, 23, 26,
-#     5, 18, 31, 10,
-#     2, 8, 24, 14,
-#     32, 27, 3, 9,
-#     19, 13, 30, 6,
-#     22, 11, 4, 25
-# ]
-
-# # Initial key permutation 1
-# key_permutation_1 = [
-#     57, 49, 41, 33, 25, 17, 9,
-#     1, 58, 50, 42, 34, 26, 18,
-#     10, 2, 59, 51, 43, 35, 27,
-#     19, 11, 3, 60, 52, 44, 36,
-#     63, 55, 47, 39, 31, 23, 15,
-#     7, 62, 54, 46, 38, 30, 22,
-#     14, 6, 61, 53, 45, 37, 29,
-#     21, 13, 5, 28, 20, 12, 4
-# ]
-
-# # Initial key permutation 2
-# key_permutation_2 = [
-#     14, 17, 11, 24, 1, 5,
-#     3, 28, 15, 6, 21, 10,
-#     23, 19, 12, 4, 26, 8,
-#     16, 7, 27, 20, 13, 2,
-#     41, 52, 31, 37, 47, 55,
-#     30, 40, 51, 45, 33, 48,
-#     44, 49, 39, 56, 34, 53,
-#     46, 42, 50, 36, 29, 32
-# ]
-
-# # S-boxes
-# s_boxes = [
-#     [
-#         [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-#         [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-#         [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-#         [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
-#     ],
-#     [
-#         [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
-#         [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
-#         [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
-#         [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
-#     ],
-#     [
-#         [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
-#         [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
-#         [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-#         [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
-#     ],
-#     [
-#         [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
-#         [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
-#         [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-#         [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
-#     ],
-#     [
-#         [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
-#         [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
-#         [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-#         [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
-#     ],
-#     [
-#         [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
-#         [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
-#         [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-#         [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
-#     ],
-#     [
-#         [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
-#         [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
-#         [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-#         [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
-#     ],
-#     [
-#         [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
-#         [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
-#         [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-#         [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
-#     ]
-# ]
-
-# # Permuted Choice 1 (PC-1)
-# def perm_choice_1(key):
-#     return [key[i - 1] for i in key_permutation_1]
-
-# # Permuted Choice 2 (PC-2)
-# def perm_choice_2(key):
-#     return [key[i - 1] for i in key_permutation_2]
-
-# # Circular left shift
-# def left_shift(key, shifts):
-#     return key[shifts:] + key[:shifts]
-
-# # Initial permutation
-# def initial_perm(text):
-#     return [text[i - 1] for i in initial_permutation]
-
-# # Final permutation
-# def final_perm(text):
-#     return [text[i - 1] for i in final_permutation]
-
-# # Expansion permutation
-# def expand_perm(text):
-#     return [text[i - 1] for i in expansion_d_box]
-
-# # P-box permutation
-# def p_box_perm(text):
-#     return [text[i - 1] for i in p_box_permutation]
-
-# # Split text into left and right halves
-# def split_text(text):
-#     split = len(text) // 2
-#     return text[:split], text[split:]
-
-# # Perform S-box substitution
-# def s_box_substitution(text):
-#     blocks = [text[i:i + 6] for i in range(0, len(text), 6)]
-#     result = []
-
-#     for i, block in enumerate(blocks):
-#         row = int(block[0] + block[5], 2)
-#         col = int(block[1:5], 2)
-#         val = s_boxes[i][row][col]
-#         result.extend(format(val, '04b'))
-
-#     return result
-
-# # DES round function
-# # DES round function (continued)
-# def des_round(left, right, round_key):
-#     expanded_right = expand_perm(right)
-#     xored = [a ^ b for a, b in zip(expanded_right, round_key)]
-#     substituted = s_box_substitution(xored)
-#     permuted = p_box_perm(substituted)
-#     new_right = [a ^ b for a, b in zip(permuted, left)]
-#     return right, new_right
-
-# # Generate round keys
-# def generate_round_keys(key):
-#     round_keys = []
-#     left, right = split_text(perm_choice_1(key))
-
-#     for round_num in range(16):
-#         # Calculate the round key for each round
-#         left = left_shift(left, shifts[round_num])
-#         right = left_shift(right, shifts[round_num])
-#         round_key = perm_choice_2(left + right)
-#         round_keys.append(round_key)
-
-#     return round_keys
-
-# # Main DES encryption function
-# def des_encrypt(plain_text, key):
-#     plain_text = initial_perm(plain_text)
-#     left, right = split_text(plain_text)
-
-#     round_keys = generate_round_keys(key)
-
-#     for round_num in range(16):
-#         left, right = des_round(left, right, round_keys[round_num])
-
-#     cipher_text = final_perm(right + left)
-#     return cipher_text
-
-# # Example usage
-# plain_text = "0111101000101010101101010111101000101010101101010111101000101010"
-# key = "1111000011001100110011111111110000110011001100110011111100001111"
-
-# cipher_text = des_encrypt(plain_text, key)
-# print("Cipher Text:", ''.join(map(str, cipher_text)))
-
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Hexadecimal to binary conversion
 def hex2bin(s):
@@ -231,11 +22,11 @@ def hex2bin(s):
         'D': "1101",
         'E': "1110",
         'F': "1111",
-        'a': "1010",  # For 'a'
-        'b': "1011",  # For 'b'
-        'c': "1100",  # For 'c'
-        'd': "1101",  # For 'd'
-        'e': "1110",  # For 'e'
+        'a': "1010",  
+        'b': "1011",  
+        'c': "1100",  
+        'd': "1101",  
+        'e': "1110", 
         'f': "1111",
         } 
         
@@ -262,12 +53,12 @@ def bin2hex(s):
         "1101": 'D',
         "1110": 'E',
         "1111": 'F',
-        "1010": 'a',  # For 'a'
-        "1011": 'b',  # For 'b'
-        "1100": 'c',  # For 'c'
-        "1101": 'd',  # For 'd'
-        "1110": 'e',  # For 'e'
-        "1111": 'f',  # For 'f'
+        "1010": 'a',
+        "1011": 'b',
+        "1100": 'c',
+        "1101": 'd',
+        "1110": 'e',
+        "1111": 'f',
         }
     hex = ""
     for i in range(0, len(s), 4):
@@ -500,7 +291,6 @@ keyp = [57, 49, 41, 33, 25, 17, 9,
         14, 6, 61, 53, 45, 37, 29,
         21, 13, 5, 28, 20, 12, 4]
 
-
 # Number of bit shifts
 shift_table = [1, 1, 2, 2,
             2, 2, 2, 2,
@@ -516,36 +306,86 @@ key_comp = [14, 17, 11, 24, 1, 5,
             30, 40, 51, 45, 33, 48,
             44, 49, 39, 56, 34, 53,
             46, 42, 50, 36, 29, 32]
+  
 
- 
+@app.route('/receive_message', methods=['POST'])
+def receive_message():
+    data = request.get_json()
+    message_text = data.get('text', '')
+    # print("Received Message:", message_text)
 
-
-# print("Encryption")
-# cipher_text = bin2hex(encrypt(plainText, rkb, rk))
-# print("Cipher Text : ", cipher_text)
-
-# print("Decryption")
-# rkb_rev = rkb[::-1]
-# rk_rev = rk[::-1]
-# text = bin2hex(encrypt(cipher_text, rkb_rev, rk_rev))
-# print("Plain Text : ", text)
-
-# This code is contributed by Aditya Jain
-
+    hex_message = string_to_combined_hex(message_text)
+    len_hex_message=len(hex_message)
     
+    print("Encryption")
+    key = "AABB09182736CCDD"
 
+    # Key generation
+    # --hex to binary
+    key = hex2bin(key)
+
+    # getting 56 bit key from 64 bit using the parity bits
+    key = permute(key, keyp, 56)
+
+    # Splitting
+    left = key[0:28] # rkb for RoundKeys in binary
+    right = key[28:56] # rk for RoundKeys in hexadecimal
+
+    rkb = []
+    rk = []
+    for i in range(0, 16):
+        # Shifting the bits by nth shifts by checking from shift table
+        left = shift_left(left, shift_table[i])
+        right = shift_left(right, shift_table[i])
+
+        # Combination of left and right string
+        combine_str = left + right
+
+        # Compression of key from 56 to 48 bits
+        round_key = permute(combine_str, key_comp, 48)
+
+        rkb.append(round_key)
+        rk.append(bin2hex(round_key))
+     
+     
+    rkb_rev = rkb[::-1]
+    rk_rev = rk[::-1]
+    string_builder=""
+    cipher_texts = []
+    for i in range(len_hex_message):
+        cipher_text = bin2hex(encrypt(hex_message[i], rkb, rk))
+        print("Cipher Text : ", cipher_text)
+        cipher_texts.append(cipher_text)
+
+        print("Decryption")
+        binary__=encrypt(cipher_text, rkb_rev, rk_rev)
+        # print("binary::",binary__)
+        text = bin2hex(encrypt(cipher_text, rkb_rev, rk_rev))
+        # print("Text : ", text)
+
+        converted_text = hex_to_text(text) 
+        string_builder=string_builder + converted_text
+        # print("Plain Text : ", converted_text)
+
+    print(string_builder)
+
+    return {'status': 'success', 'cipher_texts': cipher_texts}
+
+@app.route('/receive_message_decrypt', methods=['POST'])
 def receive_message_decrypt():
-    # data = request.get_json()
+    data = request.get_json()
+    # print("Data:",data)
+    # Accessing the 'text' field directly
+    message_texts = data.get('text', [])
 
-    # # Access the 'text' field directly
-    # text_values_nested = data.get('text', [])
-    text_values_nested=['dc743668973e9842','5a1a8c65e2298139','1ab1e7a66c01b682']
-    # hex_message = string_to_combined_hex(message_text)
+
+    text_values_nested = []
+    for message_text_list in message_texts:
+        text_values = message_text_list.get('text', [])
+        text_values_nested.append(text_values)
+
     len_hex_message=len(text_values_nested)
     print("len_hex_message:", len_hex_message)
-
-    # # Convert plain text to hexadecimal
-    # # hex_message = text_to_hex(message_text)
  
     key = "AABB09182736CCDD"
 
@@ -581,47 +421,101 @@ def receive_message_decrypt():
     rk_rev = rk[::-1]
     string_builder=""
     cipher_texts = []
- 
-    # decrypted_values_nested = []
-    # for text_values in text_values_nested:
-    #     decrypted_values = []
-    #     for cipher_text in text_values:
-    #         # print("Decryption")
-    #         binary_text = encrypt(cipher_text, rkb_rev, rk_rev)
-    #         text = bin2hex(binary_text)
-    #         converted_text = hex_to_text(text)
-    #         # print(converted_text)
-    #         # Remove null characters
-    #         cleaned_text = converted_text.replace('\x00', '')
-    #         # print(cleaned_text)
-    #         # Split words based on spaces
-    #         # words = cleaned_text.split()
-
-    #         # Concatenate words separated by commas
-    #         concatenated_words = ''.join(cleaned_text)
-    #         # print('concatenated_words:',concatenated_words)
-    #         # Append the concatenated words to the result list
-    #         decrypted_values.append(concatenated_words)
-
-    #     decrypted_values_nested.append(decrypted_values)
-    #     for val in decrypted_values_nested:
-    #         for v in val:
-    #             # print('v:',v)
-    #             word = ''.join(val)
-    #         print(word)
-            
-    decrypted_values = []
     
-    for encrypted_values in text_values_nested:
+    decrypted_values_nested = []
+    for text_values in text_values_nested:
+        decrypted_values = []
+        for cipher_text in text_values:
+            # print("Decryption")
+            binary_text = encrypt(cipher_text, rkb_rev, rk_rev)
+            text = bin2hex(binary_text)
+            converted_text = hex_to_text(text)
+            # print(converted_text)
+             
+            cleaned_text = converted_text.replace('\x00', '')
+            # print(cleaned_text)
+            # Split words based on spaces
+            # words = cleaned_text.split()
+
+            # Concatenate words separated by commas
+            concatenated_words = ''.join(cleaned_text)
+            # print('concatenated_words:',concatenated_words)
+            # Append the concatenated words to the result list
+            decrypted_values.append(concatenated_words)
+
+        decrypted_values_nested.append(decrypted_values)
+        
+    # Initialize a list to store modified messages
+    modified_messages = []    
+    # Loop through the original messages and replace the 'text' field with decrypted values
+    for i, message_text_list in enumerate(message_texts):
+        modified_message = message_text_list.copy()
+        modified_message['text'] = decrypted_values_nested[i]
+        modified_messages.append(modified_message)
+        
+    # print("modified_messages:",modified_messages)
+    return {'status': 'success', 'message_text_arr': modified_messages}
+
+@app.route('/decrypt_single_message', methods=['POST'])
+def decrypt_single_message():
+    data = request.get_json()
+    print("Data:",data) 
+    message_texts = data.get('text', [])
+    print("message_texts:",message_texts)
+   
+    len_hex_message=len(message_texts)
+    print("len_hex_message:", len_hex_message)
+
+    # # Convert plain text to hexadecimal
+    key = "AABB09182736CCDD"
+
+    # Key generation
+    # --hex to binary
+    key = hex2bin(key)
+
+    # getting 56 bit key from 64 bit using the parity bits
+    key = permute(key, keyp, 56)
+
+    # Splitting
+    left = key[0:28] # rkb for RoundKeys in binary
+    right = key[28:56] # rk for RoundKeys in hexadecimal
+
+    rkb = []
+    rk = []
+    for i in range(0, 16):
+        # Shifting the bits by nth shifts by checking from shift table
+        left = shift_left(left, shift_table[i])
+        right = shift_left(right, shift_table[i])
+
+        # Combination of left and right string
+        combine_str = left + right
+
+        # Compression of key from 56 to 48 bits
+        round_key = permute(combine_str, key_comp, 48)
+
+        rkb.append(round_key)
+        rk.append(bin2hex(round_key))
+     
+     
+    rkb_rev = rkb[::-1]
+    rk_rev = rk[::-1]
+    string_builder=""
+    cipher_texts = []
+    
+    decrypted_values = []
+    for encrypted_values in message_texts:
         binary_text = encrypt(encrypted_values, rkb_rev, rk_rev)
         text = bin2hex(binary_text)
         converted_text = hex_to_text(text)
         cleaned_text = converted_text.replace('\x00', '')
         concatenated_words = ''.join(cleaned_text)
-        print("concatenated_words:",concatenated_words)
+        # print("concatenated_words:",concatenated_words)
         decrypted_values.append(concatenated_words)
         # print("decrypted_values:",decrypted_values)
-    print("Decrypted Message Text Values Nested:", decrypted_values)
+    print("Decrypted single Message Text Values Nested:", decrypted_values)
+            
+    return {'status': 'success', 'decrypted_values': decrypted_values}
 
-
-receive_message_decrypt()
+if __name__ == '__main__':
+    app.run(debug=True)
+    
